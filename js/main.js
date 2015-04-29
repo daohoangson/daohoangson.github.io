580bc70a901377fab9c046f87e22388f7a42fbeb
Dao Hoang Son <daohoangson@gmail.com>
2015-04-30 00:20:27 +0700

    added navbar

--- js/main.js
+++ js/main.js
@@ -3,44 +3,39 @@
 	var $document = $(document);
 	var $window = $(window);
 	var windowIsTall = ($window.width() < $window.height());
-	var $headerRow = $('.header-row');
+	var $body = $('body');
+	var $profileCard = $('.profile.card');
+	var profileCardHeight = $profileCard.height();
 
-	var topBarMinHeight = function() {
-		var $topBar = $('.topbar');
-		var topBarHeight = $topBar.height();
+	var profileMinHeight = function() {
 		var windowHeight = $window.height() - $('#work').outerHeight();
-		var extraSpacing = Math.floor((windowHeight - topBarHeight) / 2);
+		var extraSpacing = Math.floor((windowHeight - profileCardHeight) / 2);
 
 		var css = '';
-		css += '.topbar { min-height: ' + windowHeight + 'px; }';
-		css += '.topbar { padding-top: ' + extraSpacing + 'px; }';
-		css += '.topbar .social { margin-top: ' + extraSpacing + 'px; }';
+		css += '.profile.card { min-height: ' + windowHeight + 'px; }';
+		css += '.profile.card { padding-top: ' + extraSpacing + 'px; }';
+		css += '.profile.card .social { margin-top: ' + extraSpacing + 'px; }';
 
 		$('<style type="text/css" id="main-js" />').text(css).appendTo($('head'));
+		profileCardHeight = Math.max(profileCardHeight, windowHeight);
 	};
 	if (!windowIsTall) {
-		topBarMinHeight();
+		profileMinHeight();
 	}
 
-	var lastScrollTop = -1;
-	var headerRowCollapsed = false;
+	var navShown = false;
 	var documentScroll = function(e) {
 		var scrollTop = $document.scrollTop();
-		var scrollDelta = scrollTop - lastScrollTop;
-		lastScrollTop = scrollTop;
 
-		if (scrollTop > 100) {
-			if (scrollDelta > 0 && !headerRowCollapsed) {
-				$headerRow.addClass('collapsed');
-				headerRowCollapsed = true;
-
-				lastScrollTop = 0;
-				$document.scrollTop(0);
+		if (scrollTop > profileCardHeight / 2) {
+			if (!navShown) {
+				$body.addClass('show-nav');
+				navShown = true;
 			}
-		} else if (scrollTop < 1) {
-			if (scrollDelta < 0 && headerRowCollapsed) {
-				$headerRow.removeClass('collapsed');
-				headerRowCollapsed = false;
+		} else {
+			if (navShown) {
+				$body.removeClass('show-nav');
+				navShown = false;
 			}
 		}
 	};
