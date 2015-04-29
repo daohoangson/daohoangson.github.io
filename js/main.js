d58d119c69f8c91c33c51f71becaf3a764301b01
Dao Hoang Son <daohoangson@gmail.com>
2015-04-30 02:48:53 +0700

    added minor UI changes

--- js/main.js
+++ js/main.js
@@ -2,27 +2,9 @@
 
 	var $document = $(document);
 	var $window = $(window);
-	var windowIsTall = ($window.width() < $window.height());
 	var $body = $('body');
 	var $nav = $('nav');
-	var $profileCard = $('.profile.card');
-	var profileCardHeight = $profileCard.height();
-
-	var profileMinHeight = function() {
-		var windowHeight = $window.height() - $('#work').outerHeight();
-		var extraSpacing = Math.floor((windowHeight - profileCardHeight) / 2);
-
-		var css = '';
-		css += '.profile.card { min-height: ' + windowHeight + 'px; }';
-		css += '.profile.card { padding-top: ' + extraSpacing + 'px; }';
-		css += '.profile.card .social { margin-top: ' + extraSpacing + 'px; }';
-
-		$('<style type="text/css" id="main-js" />').text(css).appendTo($('head'));
-		profileCardHeight = Math.max(profileCardHeight, windowHeight);
-	};
-	if (!windowIsTall) {
-		profileMinHeight();
-	}
+	var profileCardHeight = $('.profile.card').height();
 
 	var navShown = false;
 	var documentScroll = function(e) {
